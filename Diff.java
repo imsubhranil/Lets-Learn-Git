03494a0a50d768e572c9a7b24d46ff60e09088c2
imsubhranil <subhranil.samanta@jktech.com>
2023-02-11 22:54:42 +0530

    Adding main function code in Diff.java

--- Diff.java
+++ Diff.java
@@ -1,3 +1,8 @@
 public class Diff {
-    
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+
+        System.out.println("The difference is = " + (x-y));
+    }
 }
